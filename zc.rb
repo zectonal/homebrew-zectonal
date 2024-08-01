eea407c97386848f326ff56acf2c99b3cd0d9203
zectdev <developer@zectonal.com>
2024-08-01 15:11:07 -0400

    Update zc and zect to version 0.15.0

---
+++ zc.rb
@@ -0,0 +1,15 @@
+class Zc < Formula
+  desc "command line data quality monitoring that is blazingly fast and easy to use."
+  homepage "https://www.zectonal.com"
+  url "https://github.com/zectonal/zectonal/releases/download/v0.15.0/zc-mac.zip"
+  sha256 "939294c400882deef2879457a598a9132a4dba8550d393ac481b56fe5f092a85" # Replace with the SHA256 hash of your zc binary
+  version "0.15.0"
+
+  def install
+    bin.install "zc"
+  end
+
+  test do
+    system "#{bin}/zc", "--version"
+  end
+end
---
+++ zect.rb
@@ -0,0 +1,34 @@
+class Zect < Formula
+  desc "zect data quality monitoring and AI-derived root cause analysis"
+  homepage "https://www.zectonal.com"
+  url "https://github.com/zectonal/zectonal/releases/download/v0.15.0/zect-mac.zip"
+  sha256 "9db7d2d35fb8ab4c7bf72a0022f1e8884a723a757fa1a25687caf28efb8625a4"
+  version "0.15.0"
+
+  def install
+    bin.install "zect"
+  end
+
+  service do
+    run [opt_bin/"zect", "run"]
+    keep_alive true
+    working_dir var
+    log_path var/"log/zect.log"
+    error_log_path var/"log/zect.error.log"
+  end
+  def caveats
+    <<~EOS
+      To completely remove Zect and all its data, run:
+        brew uninstall zect
+        rm -rf #{var}/log/zect*
+        rm -rf ~/.config/zect
+    EOS
+  end
+  def post_uninstall
+    rm_rf var/"log/zect*"
+    rm_rf "~/.config/zect"
+  end
+  test do
+    system "#{bin}/zect", "--version"
+  end
+end
