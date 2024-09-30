6ce67a433bef8f94bceadb177505ce22865dbc85
zectdev <dev@zect.dev>
2024-09-30 16:44:28 -0400

    Update zc and zect to version 0.17.1

--- zc.rb
+++ zc.rb
@@ -1,15 +1,15 @@
 class Zc < Formula
   desc "Command line data quality monitoring that is blazingly fast and easy to use."
   homepage "https://github.com/zectonal/homebrew-zectonal"
-  version "0.17.0"
+  version "0.17.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/zectonal/homebrew-zectonal/releases/download/v0.17.0/zc_0.17.0_macos.zip"
-      sha256 "1f688eca5f17d16de7a73abd61e5d38cc1ec0f2d995b35376a5e60200420a59b"
+      url "https://github.com/zectonal/homebrew-zectonal/releases/download/v0.17.1/zc_0.17.1_macos.zip"
+      sha256 "d45dbdefd285585c8e6c632658f8fa4eda60e96e03f934d2b4f1b6680ac015fc"
     else
-      url "https://github.com/zectonal/homebrew-zectonal/releases/download/v0.17.0/zc_0.17.0_macos_x86.zip"
-      sha256 "f46c5d626ea4b8d1d025005f3a0e9254ac61428c183697339caa2af4bd7bd295"
+      url "https://github.com/zectonal/homebrew-zectonal/releases/download/v0.17.1/zc_0.17.1_macos_x86.zip"
+      sha256 "5c74370b0d48017935708c125e68fc76255e94dcfd750be26c3a6c87c3bd6a51"
     end
   end
 
--- zect.rb
+++ zect.rb
@@ -1,15 +1,15 @@
 class Zect < Formula
   desc "zect data quality monitoring and AI-derived root cause analysis"
   homepage "https://github.com/zectonal/homebrew-zectonal"
-  version "0.17.0"
+  version "0.17.1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/zectonal/homebrew-zectonal/releases/download/v0.17.0/zect_0.17.0_macos.zip"
-      sha256 "40e21385ebe82ad7b489bff7f317cac99370c5dac1bc641007922a7db62f9045"
+      url "https://github.com/zectonal/homebrew-zectonal/releases/download/v0.17.1/zect_0.17.1_macos.zip"
+      sha256 "c8d434f1531d8ad83624ab915b0c5f7c18edf3321840df5f25d9f289bf1402c8"
     else
-      url "https://github.com/zectonal/homebrew-zectonal/releases/download/v0.17.0/zect_0.17.0_macos_x86.zip"
-      sha256 "47ee29a077882e8c255a0f62751f07cab65a7aa3c07a37592e0c7806a7bc3842"
+      url "https://github.com/zectonal/homebrew-zectonal/releases/download/v0.17.1/zect_0.17.1_macos_x86.zip"
+      sha256 "a85f3db6e99911d3377ad303e93a5d704a93c5102ef6cfcffa2619f64ee78966"
     end
   end
   
